4850624413114dd5df1d312736de55e775b3b3e9
Park Byeong-Jun <45068233+LaSeoal@users.noreply.github.com>
2019-02-01 17:18:08 +0900

    Update Music.java

--- project/Music.java
+++ project/Music.java
@@ -6,59 +6,49 @@ import java.io.FileInputStream;
 
 import javazoom.jl.player.Player;
 
-public class Music extends Thread
-{
+public class Music extends Thread {
 	private Player player;
 	private boolean isLoop;
 	private File file;
 	private FileInputStream fis;
 	private BufferedInputStream bis;
 	
-	public Music(String name, boolean isLoop)
-	{
-		try
-		{
+	public Music(String name, boolean isLoop) {
+		try {
 			this.isLoop=isLoop;
-			file=new File(Main.class.getResource("../music/"+name).toURI());
+			file=new File(getClass().getClassLoader().getResource(name).toURI());
 			fis=new FileInputStream(file);
 			bis=new BufferedInputStream(fis);
 			player=new Player(bis);
 		} 
-		catch(Exception e)
-		{
+		catch(Exception e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	public int getTime()
-	{
+	public int getTime(){
 		if(player==null)
 			return 0;
 		return player.getPosition();
 	}
 	
-	public void close()
-	{
+	public void close(){
 		isLoop=false;
 		player.close();
 		this.interrupt();
 	}
 	
-	@Override//thread»ó¼Ó½Ã ÇÊ¼ö
-	public void run()
-	{
-		try
-		{
-			do
-			{
+	@Override//threadìƒì†ì‹œ í•„ìˆ˜
+	public void run(){
+		try{
+			do{
 				player.play();
 				fis=new FileInputStream(file);
 				bis=new BufferedInputStream(fis);
 				player=new Player(bis);
 			}while(isLoop);
 		}
-		catch(Exception e)
-		{
+		catch(Exception e){
 			System.out.println(e.getMessage());
 		}
 	}
