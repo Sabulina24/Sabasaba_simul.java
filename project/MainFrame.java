6e0331377e306820c1a0fffef55eb00be7fc93cf
Park Byeong-Jun <45068233+LaSeoal@users.noreply.github.com>
2019-02-01 17:17:42 +0900

    Update MainFrame.java

--- project/MainFrame.java
+++ project/MainFrame.java
@@ -1,28 +1,21 @@
 package project;
 
-//import java.awt.Toolkit;
 import javax.swing.JFrame;
 
-public class mainFrame extends JFrame{
+public class MainFrame extends JFrame{
 
 	private static final long serialVersionUID = 1L;
 
-	public mainFrame()
-	{
-		//Toolkit kit = Toolkit.getDefaultToolkit();
+	public MainFrame(){
 		setSize(1280, 720);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("교수님 사바사바 시뮬레이션");
 		setVisible(true);
 		setResizable(false); // 화면 크기 임의 조정 불가
 		setLocationRelativeTo(null); // 화면 정중앙에 고정
-		//Image gameIcon - kit.getImage("icon.gif");
-		//setIconImage(gameIcon);
 	}
 	
-	public void exitFrame()
-	{
+	public void exitFrame(){
 		setVisible(false);
-	}
-	
+	}	
 }
