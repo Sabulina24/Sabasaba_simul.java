494339f54d71a77f2666c715a74e55fe64858e5b
Park Byeong-Jun <45068233+LaSeoal@users.noreply.github.com>
2019-02-01 17:13:16 +0900

    Update Dialogue_C.java

--- project/Dialogue_C.java
+++ project/Dialogue_C.java
@@ -5,48 +5,38 @@ public class Dialogue_C {
 	boolean[] isCalled = new boolean[3];
 	public static int n = 0;
 	
-	public void getDialogue(mainFrame f)
-	{
+	public void getDialogue(MainFrame f){
 		boolean isDialogueWorked = false;
 		
-		if(isSpecialDialogue == true)
-		{
+		if(isSpecialDialogue == true){
 			C_Special b = new C_Special();
 			f.add(b);
 			n = 3;
 			isDialogueWorked = true;
 			isSpecialDialogue = false;
 		}
-		else
-		{
-			do
-			{
+		else{
+			do{
 				n = (int)(Math.random()*4);
 			
-				if(n == 0)
-				{
-					if(isCalled[n] == false)
-					{
+				if(n == 0){
+					if(isCalled[n] == false){
 						C_Normal_1 b = new C_Normal_1();
 						f.add(b);
 						isCalled[n] = true;
 						isDialogueWorked = true;		
 					}
 				}
-				if(n == 1)
-				{
-					if(isCalled[n] == false)
-					{
+				if(n == 1){
+					if(isCalled[n] == false){
 						C_Normal_2 b = new C_Normal_2();
 						f.add(b);
 						isCalled[n] = true;
 						isDialogueWorked = true;
 					}
 				}
-				if(n == 2)
-				{
-					if(isCalled[n] == false)
-					{
+				if(n == 2){
+					if(isCalled[n] == false){
 						C_Normal_3 b = new C_Normal_3();
 						f.add(b);
 						isCalled[n] = true;
@@ -57,8 +47,7 @@ public class Dialogue_C {
 		}
 	}
 	
-	public void getDialogue_Holiday(mainFrame f)
-	{
+	public void getDialogue_Holiday(MainFrame f){
 		C_Holiday b = new C_Holiday();
 		f.add(b);
 		n = 4;
