90dcf6c457fdb4d1fa92509facbecaab7aaaa3ba
Park Byeong-Jun <45068233+LaSeoal@users.noreply.github.com>
2019-02-01 17:12:37 +0900

    Update Dialogue_B.java

--- project/Dialogue_B.java
+++ project/Dialogue_B.java
@@ -1,68 +1,53 @@
 package project;
 
 public class Dialogue_B {
-
+	
 	public static boolean isSpecialDialogue = false;
 	boolean[] isCalled = new boolean[3];
 	public static int n = 0;
 	
-	public void getDialogue(mainFrame f)
-	{
+	public void getDialogue(MainFrame f) {
 		boolean isDialogueWorked = false;
 		
-		if(isSpecialDialogue == true)
-		{
-			// Special ´ëÈ­ ºÒ·¯¿À±â
-			
+		if(isSpecialDialogue == true) { // Special ëŒ€í™” ë¶ˆëŸ¬ì˜¤ê¸°
 			B_Special s = new B_Special();
 			f.add(s);
 			n = 3;
 			isDialogueWorked = true;
 			isSpecialDialogue = false;
-		}
-		else
-		{
-			do
-			{
+		} else {
+			do {
 				n = (int)(Math.random()*4);
 			
-				if(n == 0)
-				{
-					if(isCalled[n] == false)
-					{
+				if(n == 0) {
+					if(isCalled[n] == false) {
 						B_Normal_1 b = new B_Normal_1();
 						f.add(b);
 						isCalled[n] = true;
 						isDialogueWorked = true;
 					}
 				}
-				if(n == 1)
-				{
-					if(isCalled[n] == false)
-					{
+				if(n == 1) {
+					if(isCalled[n] == false) {
 						B_Normal_2 b = new B_Normal_2();
 						f.add(b);
 						isCalled[n] = true;
 						isDialogueWorked = true;
 					}
 				}
-				if(n == 2)
-				{
-					if(isCalled[n] == false)
-					{
+				if(n == 2) {
+					if(isCalled[n] == false) {
 						B_Normal_3 b = new B_Normal_3();
 						f.add(b);
 						isCalled[n] = true;
 						isDialogueWorked = true;
 					}
 				}
-		}while(isDialogueWorked == false);
+		} while(isDialogueWorked == false);
 		}
 	}
 	
-	public void getDialogue_Holiday(mainFrame f)
-	{
-		// Holiday ´ëÈ­ ºÒ·¯¿À±â
+	public void getDialogue_Holiday(MainFrame f) { // Holiday ëŒ€í™” ë¶ˆëŸ¬ì˜¤ê¸°
 		B_Holiday b = new B_Holiday();
 		f.add(b);
 		n = 4;
