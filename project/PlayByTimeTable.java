74ffd04e695d613b31d6a95f76dde03552b2c086
Park Byeong-Jun <45068233+LaSeoal@users.noreply.github.com>
2019-02-01 17:18:37 +0900

    Update PlayByTimeTable.java

--- project/PlayByTimeTable.java
+++ project/PlayByTimeTable.java
@@ -3,11 +3,12 @@ package project;
 import java.awt.image.BufferedImage;
 
 public class PlayByTimeTable {
-
+	ClassLoader cl = this.getClass().getClassLoader();
 	int[][] table = TimeTable.settable();
 	// 교수님A : 1,2,3	교수님B : 6,7,8	교수님C : 11,12,13
 	
-	MainFrame f = new mainFrame();
+	MainFrame f = new MainFrame();
+	
 	Dialogue_A a = new Dialogue_A();
 	Dialogue_B b = new Dialogue_B();
 	Dialogue_C c = new Dialogue_C();
@@ -16,27 +17,18 @@ public class PlayByTimeTable {
 	
 	private static int n = 0;
 	
-	public PlayByTimeTable()
-	{
-		Dialogue_C.n = 4;
-		c.getDialogue_Holiday(f);
-		f.setVisible(true);
-		w.IsWork_C();
-		
-		/*
-		f.add(new Simul_start());
+	public PlayByTimeTable() {
+		f.add(new SimulStart());
 		f.setVisible(true);
 		
 		w.Start();
 		
-		for(int i = 0 ; i < 5; i++)
-		{
+		for(int i = 0 ; i < 5; i++) {
 			System.out.println("하루시작");
 			Background z = new Background();
 			f.add(z);
 			
-			if(table[0][i] == 0 && table[1][i] == 0 && table[2][i] == 0) // 풀공강 일 때
-			{
+			if(table[0][i] == 0 && table[1][i] == 0 && table[2][i] == 0) { // 풀공강 일 때
 				z.getBackground(i);
 				z.getHoliday();
 				
@@ -45,30 +37,26 @@ public class PlayByTimeTable {
 				
 				// 휴일 대화를 불러온다.
 				n = (int)(Math.random()*3);
-				if(n == 0)
-				{
+				if(n == 0) {
 					Dialogue_A.n = 4;
 					a.getDialogue_Holiday(f);
 					f.setVisible(true);
 					w.IsWork_A();
 				}
-				else if(n == 1)
-				{
+				else if(n == 1) {
 					Dialogue_B.n = 4;
 					b.getDialogue_Holiday(f);
 					f.setVisible(true);
 					w.IsWork_B();
 				}
-				else
-				{
+				else {
 					Dialogue_C.n = 4;
 					c.getDialogue_Holiday(f);
 					f.setVisible(true);
 					w.IsWork_C();
 				}
 			}
-			else
-			{
+			else {
 				z.getBackground(i);
 				int x1 = table[0][i];
 				int x2 = table[1][i];
@@ -81,22 +69,18 @@ public class PlayByTimeTable {
 				Background.isWorked = false;
 			}
 			
-			for(int j = 0 ; j < 3; j++)
-			{
-				if(table[j][i] == 1 || table[j][i] == 2 || table[j][i] == 3)
-				{
+			for(int j = 0 ; j < 3; j++) {
+				if(table[j][i] == 1 || table[j][i] == 2 || table[j][i] == 3) {
 					a.getDialogue(f);
 					f.setVisible(true);
 					w.IsWork_A();
 				}
-				else if(table[j][i] == 6 || table[j][i] == 7 || table[j][i] == 8)
-				{
+				else if(table[j][i] == 6 || table[j][i] == 7 || table[j][i] == 8) {
 					b.getDialogue(f);
 					f.setVisible(true);
 					w.IsWork_B();
 				}
-				else if(table[j][i] == 11 || table[j][i] == 12 || table[j][i] == 13)
-				{
+				else if(table[j][i] == 11 || table[j][i] == 12 || table[j][i] == 13) {
 					c.getDialogue(f);
 					f.setVisible(true);
 					w.IsWork_C();
@@ -119,22 +103,19 @@ public class PlayByTimeTable {
 		w.IsWork_Drinking();
 	
 		
-		if(DrinkingGame.n == 1)
-		{
+		if(DrinkingGame.n == 1) {
 			f.add(new profA());
 			f.setVisible(true);
 			
 			w.Drink_A();
 		}
-		else if(DrinkingGame.n == 2)
-		{
+		else if(DrinkingGame.n == 2) {
 			f.add(new profB());
 			f.setVisible(true);
 			
 			w.Drink_B();
 		}
-		else
-		{
+		else {
 			f.add(new profC());
 			f.setVisible(true);
 			
@@ -163,359 +144,263 @@ public class PlayByTimeTable {
 		//게임 결과 창 실행
 		f.add(new GameResult());
 		f.setVisible(true);
-		*/
 	}
 }
 
 class Work{
 	
-	public void IsWork_day()
-	{
+	public void IsWork_day() {
 		Day();
 	}
-	public void IsWork_A()
-	{
-		if(Dialogue_A.n == 0)
-		{
+	public void IsWork_A() {
+		if(Dialogue_A.n == 0) {
 			ANormal_1();
-		}
-		else if(Dialogue_A.n == 1)
-		{
+		} 
+		else if(Dialogue_A.n == 1) {
 			ANormal_2();
 		}
-		else if(Dialogue_A.n == 2)
-		{
+		else if(Dialogue_A.n == 2) {
 			ANormal_3();
 		}
-		else if(Dialogue_A.n == 3)
-		{
+		else if(Dialogue_A.n == 3) {
 			ASpecial();
 		}
-		else if(Dialogue_A.n == 4)
-		{
+		else if(Dialogue_A.n == 4) {
 			AHoliday();
 		}
 	}
-	public void IsWork_B()
-	{
-		if(Dialogue_B.n == 0)
-		{
+	public void IsWork_B() {
+		if(Dialogue_B.n == 0) {
 			BNormal_1();
 		}
-		else if(Dialogue_B.n == 1)
-		{
+		else if(Dialogue_B.n == 1) {
 			BNormal_2();
 		}
-		else if(Dialogue_B.n == 2)
-		{
+		else if(Dialogue_B.n == 2) {
 			BNormal_3();
 		}
-		else if(Dialogue_B.n == 3)
-		{
+		else if(Dialogue_B.n == 3) {
 			BSpecial();
 		}
-		else if(Dialogue_B.n == 4)
-		{
+		else if(Dialogue_B.n == 4) {
 			BHoliday();
 		}
 	}
-	public void IsWork_C()
-	{
-		if(Dialogue_C.n == 0)
-		{
+	public void IsWork_C() {
+		if(Dialogue_C.n == 0) {
 			CNormal_1();
 		}
-		else if(Dialogue_C.n == 1)
-		{
+		else if(Dialogue_C.n == 1) {
 			CNormal_2();
 		}
-		else if(Dialogue_C.n == 2)
-		{
+		else if(Dialogue_C.n == 2) {
 			CNormal_3();
 		}
-		else if(Dialogue_C.n == 3)
-		{
+		else if(Dialogue_C.n == 3) {
 			CSpecial();
 		}
-		else if(Dialogue_C.n == 4)
-		{
+		else if(Dialogue_C.n == 4) {
 			CHoliday();
 		}
 	}
-	public void IsWork_Drinking()
-	{
+	public void IsWork_Drinking() {
 		Drinking();
 	}
-	public void IsWork_Drink_A()
-	{
+	public void IsWork_Drink_A() {
 		Drink_A();
 	}
-	public void IsWork_Drink_B()
-	{
+	public void IsWork_Drink_B() {
 		Drink_B();
 	}
-	public void IsWork_Drink_C()
-	{
+	public void IsWork_Drink_C() {
 		Drink_C();
 	}
-	public void IsWork_Sleep()
-	{
+	public void IsWork_Sleep() {
 		Sleep();
 	}
-	public void IsWork_SleepPlay()
-	{
+	public void IsWork_SleepPlay() {
 		SleepPlay();
 	}
-	public synchronized void Sleep()
-	{
-		while(TryNotToSleep.isWorked == false)
-		{
-			try
-			{
+	public synchronized void Sleep() {
+		while(TryNotToSleep.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void SleepPlay()
-	{
-		while(TryNotToSleep_Play.isWorked == false)
-		{
-			try
-			{
+	public synchronized void SleepPlay() {
+		while(TryNotToSleep_Play.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void Drink_A()
-	{
-		while(profA.isWorked == false)
-		{
-			try
-			{
+	public synchronized void Drink_A() {
+		while(profA.isWorked == false) {
+			try	{
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void Drink_B()
-	{
-		while(profB.isWorked == false)
-		{
-			try
-			{
+	public synchronized void Drink_B() {
+		while(profB.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void Drink_C()
-	{
-		while(profC.isWorked == false)
-		{
-			try
-			{
+	public synchronized void Drink_C() {
+		while(profC.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void Drinking()
-	{
-		while(DrinkingGame.isWorked == false)
-		{
-			try
-			{
+	public synchronized void Drinking() {
+		while(DrinkingGame.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void Day()
-	{
-		while(Background.isWorked == false)
-		{
-			try
-			{
+	public synchronized void Day() {
+		while(Background.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void Start()
-	{
-		while(Simul_start.isWorked == false)
-		{
-			try
-			{
+	public synchronized void Start() {
+		while(SimulStart.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void ANormal_1()
-	{
-		while(A_Normal_1.isWorked == false)
-		{
-			try
-			{
+	public synchronized void ANormal_1() {
+		while(A_Normal_1.isWorked == false) {
+			try	{
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void ANormal_2()
-	{
-		while(A_Normal_2.isWorked == false)
-		{
-			try
-			{
+	public synchronized void ANormal_2() {
+		while(A_Normal_2.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void ANormal_3()
-	{
-		while(A_Normal_3.isWorked == false)
-		{
-			try
-			{
+	public synchronized void ANormal_3() {
+		while(A_Normal_3.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void ASpecial()
-	{
-		while(A_Special.isWorked == false)
-		{
-			try
-			{
+	public synchronized void ASpecial() {
+		while(A_Special.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void AHoliday()
-	{
-		while(A_Holiday.isWorked == false)
-		{
-			try
-			{
+	public synchronized void AHoliday() {
+		while(A_Holiday.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void BNormal_1()
-	{
-		while(B_Normal_1.isWorked == false)
-		{
-			try
-			{
+	public synchronized void BNormal_1() {
+		while(B_Normal_1.isWorked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void BNormal_2()
-	{
-		while(B_Normal_2.isWorked == false)
-		{
-			try
-			{
+	public synchronized void BNormal_2() {
+		while(B_Normal_2.isWorked == false) {
+			try {
+				Thread.sleep(50);
+			}
+			catch(InterruptedException e) {}
+		}
+	}
+	public synchronized void BNormal_3() {
+		while(B_Normal_3.isWorked == false) {
+			try {
+				Thread.sleep(50);
+			}
+			catch(InterruptedException e) {System.out.println("예외");}
+		}
+	}
+	public synchronized void BSpecial() {
+		while(B_Special.isWorked == false) {
+			try {
+				Thread.sleep(50);
+			}
+			catch(InterruptedException e) {System.out.println("예외");}
+		}
+	}
+	public synchronized void BHoliday() {
+		while(B_Holiday.isWorked == false) {
+			try {
+				Thread.sleep(50);
+			}
+			catch(InterruptedException e) {System.out.println("예외");}
+		}
+	}
+	public synchronized void CNormal_1() {
+		while(C_Normal_1.isworked == false) {
+			try {
+				Thread.sleep(50);
+			}
+			catch(InterruptedException e) {System.out.println("예외");}
+		}
+	}
+	public synchronized void CNormal_2() {
+		while(C_Normal_2.isworked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {}
 
 		}
 	}
-	public synchronized void BNormal_3()
-	{
-		while(B_Normal_3.isWorked == false)
-		{
-			try
-			{
+	public synchronized void CNormal_3() {
+		while(C_Normal_3.isworked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void BSpecial()
-	{
-		while(B_Special.isWorked == false)
-		{
-			try
-			{
+	public synchronized void CSpecial() {
+		while(C_Special.isworked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
 		}
 	}
-	public synchronized void BHoliday()
-	{
-		while(B_Holiday.isWorked == false)
-		{
-			try
-			{
-				Thread.sleep(50);
-			}
-			catch(InterruptedException e) {System.out.println("예외");}
-		}
-	}
-	public synchronized void CNormal_1()
-	{
-		while(C_Normal_1.isworked == false)
-		{
-			try
-			{
-				Thread.sleep(50);
-			}
-			catch(InterruptedException e) {System.out.println("예외");}
-		}
-	}
-	public synchronized void CNormal_2()
-	{
-		while(C_Normal_2.isworked == false)
-		{
-			try
-			{
-				Thread.sleep(50);
-			}
-			catch(InterruptedException e) {}
-
-		}
-	}
-	public synchronized void CNormal_3()
-	{
-		while(C_Normal_3.isworked == false)
-		{
-			try
-			{
-				Thread.sleep(50);
-			}
-			catch(InterruptedException e) {System.out.println("예외");}
-		}
-	}
-	public synchronized void CSpecial()
-	{
-		while(C_Special.isworked == false)
-		{
-			try
-			{
-				Thread.sleep(50);
-			}
-			catch(InterruptedException e) {System.out.println("예외");}
-		}
-	}
-	public synchronized void CHoliday()
-	{
-		while(C_Holiday.isworked == false)
-		{
-			try
-			{
+	public synchronized void CHoliday() {
+		while(C_Holiday.isworked == false) {
+			try {
 				Thread.sleep(50);
 			}
 			catch(InterruptedException e) {System.out.println("예외");}
