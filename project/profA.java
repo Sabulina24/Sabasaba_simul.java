2d766763a1af2708eeeffe07f62142afd46ecdf6
Park Byeong-Jun <45068233+LaSeoal@users.noreply.github.com>
2019-02-01 17:25:15 +0900

    Update profA.java

--- project/profA.java
+++ project/profA.java
@@ -24,10 +24,10 @@ public class profA extends JPanel {
 		
 		setLayout(null);
 		
-		JButton button_1 = new JButton("\uB2E4\uC74C\uC73C\uB85C");
+		final JButton button_1 = new JButton("\uB2E4\uC74C\uC73C\uB85C");
 			button_1.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent arg0) {
-					//°á°ú È­¸éÀ¸·Î ³Ñ¾î°£´Ù.
+					//ê²°ê³¼ í™”ë©´ìœ¼ë¡œ ë„˜ì–´ê°„ë‹¤.
 					setVisible(false);
 					isWorked = true;
 				}
@@ -37,32 +37,32 @@ public class profA extends JPanel {
 		add(button_1);
 		button_1.setVisible(false);
 		
-		JLabel lblNewLabel_2 = new JLabel(".");
+		final JLabel lblNewLabel_2 = new JLabel(".");
 		lblNewLabel_2.setBounds(0, 0, 64, 24);
 		add(lblNewLabel_2);
-		JLabel lblNewLabel_1 = new JLabel("");
+		final JLabel lblNewLabel_1 = new JLabel("");
 		
 		
 		lblNewLabel_1.setBounds(1100, 165, 96, 93);
 		add(lblNewLabel_1);
 		
-		JLabel label_2 = new JLabel("");
+		final JLabel label_2 = new JLabel("");
 		
 		label_2.setBounds(130, 165, 96, 93);
 		add(label_2);
 		
-		JButton button_2 = new JButton("\uB0B4\uB824\uB193\uAE30");
+		final JButton button_2 = new JButton("\uB0B4\uB824\uB193\uAE30");
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				lblNewLabel.setIcon(new ImageIcon(profA.class.getResource("../images/\uC220\uC790\uB9AC(\uC548\uB9C8\uC2EC)_A.jpg")));//******
+				lblNewLabel.setIcon(new ImageIcon(getClass().getClassLoader().getResource("\uC220\uC790\uB9AC(\uC548\uB9C8\uC2EC)_A.jpg")));//******
 			}
 		});
 		button_2.setBounds(533, 557, 260, 93);
 		add(button_2);
 		
-		JButton btnNewButton = new JButton("\uB9C8\uC2E0\uB2E4");
+		final JButton btnNewButton = new JButton("\uB9C8\uC2E0\uB2E4");
 		
-		JButton button = new JButton("\uC548\uB9C8\uC2E0\uB2E4");
+		final JButton button = new JButton("\uC548\uB9C8\uC2E0\uB2E4");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
@@ -73,52 +73,36 @@ public class profA extends JPanel {
 				Meter=DrinkingAlgorithm.Drink(a);
 				
 				
-				if(Meter<5)
-					{
-							lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/0-5.png")));
-							
-					
-					}
-				else if(Meter>=5&&Meter<10)
-					
-					{
-						lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/5-10.png")));
-						
-					
-					}
-				else if(Meter>=10&&Meter<15)
-					
-					{
-						lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/10-15.png")));
-						
-					
-					}
-				else if(Meter>=15&&Meter<20)
-				{
-						
-						
-					lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/19.png")));
-					
-				
+				if(Meter<5){
+							lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("0-5.png")));
+				}
+				else if(Meter>=5&&Meter<10){
+						lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("5-10.png")));
+				}
+				else if(Meter>=10&&Meter<15){
+						lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("10-15.png")));
+				}
+				else if(Meter>=15&&Meter<20){
+					lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("19.png")));
 				}
 				if(DrinkingAlgorithm.Mood<3)
-					label_2.setIcon(new ImageIcon(profA.class.getResource("../images/19.png")));
+					label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("19.png")));
 						
 				else if(DrinkingAlgorithm.Mood>=3&&DrinkingAlgorithm.Mood<6)
 						
-					label_2.setIcon(new ImageIcon(profA.class.getResource("../images/10-15.png")));
+					label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("10-15.png")));
 					
 				else if(DrinkingAlgorithm.Mood>=6&&DrinkingAlgorithm.Mood<=9)
 						
-					label_2.setIcon(new ImageIcon(profA.class.getResource("../images/5-10.png")));
+					label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("5-10.png")));
 					
 				else if(DrinkingAlgorithm.Mood>=10)
-					label_2.setIcon(new ImageIcon(profA.class.getResource("../images/0-5.png")));
+					label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("0-5.png")));
 				
 				if(Meter<5&&DrinkingAlgorithm.Mood>=10) {
 					lblNewLabel_2.setBounds(0, 0, 1280, 720);
-					lblNewLabel_2.setIcon(new ImageIcon(profA.class.getResource("../images/win1.png")));
-					GoodFeeling.teacher1FeelUp(); //±³¼ö aÈ£°¨µµ »ó½Â
+					lblNewLabel_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("win1.png")));
+					Feeling.pf1_UP(); //êµìˆ˜ aí˜¸ê°ë„ ìƒìŠ¹
 					btnNewButton.setVisible(false);
 					button_2.setVisible(false);
 					button.setVisible(false);
@@ -140,78 +124,57 @@ public class profA extends JPanel {
 				
 				Meter=DrinkingAlgorithm.Drink(a);
 				
-				if(Meter<5)
-					{
-							lblNewLabel.setIcon(new ImageIcon(profA.class.getResource("../images/\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//********************
-							
-							lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/0-5.png")));
-							
-				add(btnNewButton);
-					
-					}
-				else if(Meter>=5&&Meter<10)
-					
-					{
-							lblNewLabel.setIcon(new ImageIcon(profA.class.getResource("../images/\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//**************
-							
-						lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/5-10.png")));
-						
-					
-					}
-				else if(Meter>=10&&Meter<15)
-					
-					{
-							lblNewLabel.setIcon(new ImageIcon(profA.class.getResource("../images/\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//********
-							
-						
-						lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/10-15.png")));
-						
-					
-					}
-				else if(Meter>=15&&Meter<20)
-				{
-						lblNewLabel.setIcon(new ImageIcon(profA.class.getResource("../images/\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//**
-						
-					lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/19.png")));
-					
-				
-					
+				if(Meter<5){
+					lblNewLabel.setIcon(new ImageIcon(getClass().getClassLoader().getResource("\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//********************
+					lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("0-5.png")));
+					add(btnNewButton);
 				}
-				else if(Meter>=20)
-					{
+				else if(Meter>=5&&Meter<10){
+					lblNewLabel.setIcon(new ImageIcon(getClass().getClassLoader().getResource("\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//**************	
+					lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("5-10.png")));
+				}
+				else if(Meter>=10&&Meter<15){
+					lblNewLabel.setIcon(new ImageIcon(getClass().getClassLoader().getResource("\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//********
+					lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("10-15.png")));
+				}
+				else if(Meter>=15&&Meter<20){
+					lblNewLabel.setIcon(new ImageIcon(getClass().getClassLoader().getResource("\uC220\uC790\uB9AC(\uB9C8\uC2EC)_A.jpg")));//**
+					lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("19.png")));
+				}
+				else if(Meter>=20){
 					lblNewLabel_2.setBounds(0, 0, 1280, 720);
-					lblNewLabel_2.setIcon(new ImageIcon(profA.class.getResource("../images/lose1.png")));//Áö´Â È­¸é
-					GoodFeeling.teacher1FeelDown(); //±³¼ö aÈ£°¨µµ °¨¼Ò
-					btnNewButton.setVisible(false); //¸¶½Ã±â ¹öÆ° ¾Èº¸ÀÌ°ÔÇÑ´Ù
-					button.setVisible(false); //¾È¸¶½Ã±â ¹öÆ° ¾Èº¸ÀÌ°ÔÇÑ´Ù
-					button_2.setVisible(false); //³»¸®±â ¹öÆ° ¾Èº¸ÀÌ°ÔÇÑ´Ù
+					lblNewLabel_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("lose1.png")));//ì§€ëŠ” í™”ë©´
+					Feeling.pf1_DOWN(); //êµìˆ˜ aí˜¸ê°ë„ ê°ì†Œ
+					btnNewButton.setVisible(false); //ë§ˆì‹œê¸° ë²„íŠ¼ ì•ˆë³´ì´ê²Œí•œë‹¤
+					button.setVisible(false); //ì•ˆë§ˆì‹œê¸° ë²„íŠ¼ ì•ˆë³´ì´ê²Œí•œë‹¤
+					button_2.setVisible(false); //ë‚´ë¦¬ê¸° ë²„íŠ¼ ì•ˆë³´ì´ê²Œí•œë‹¤
 					
-					button_1.setVisible(true); //°á°úÈ­¸éÀ¸·Î ³Ñ¾î°¡´Â ¹öÆ° º¸ÀÌ°ÔÇÑ´Ù
-					}
+					button_1.setVisible(true); //ê²°ê³¼í™”ë©´ìœ¼ë¡œ ë„˜ì–´ê°€ëŠ” ë²„íŠ¼ ë³´ì´ê²Œí•œë‹¤
+				}
 				
 			if(DrinkingAlgorithm.Mood<3)
-				label_2.setIcon(new ImageIcon(profA.class.getResource("../images/19.png")));
+				label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("19.png")));
 					
 			else if(DrinkingAlgorithm.Mood>=3&&DrinkingAlgorithm.Mood<6)
 					
-				label_2.setIcon(new ImageIcon(profA.class.getResource("../images/10-15.png")));
+				label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("10-15.png")));
 				
 			else if(DrinkingAlgorithm.Mood>=6&&DrinkingAlgorithm.Mood<=9)
 					
-				label_2.setIcon(new ImageIcon(profA.class.getResource("../images/5-10.png")));
+				label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("5-10.png")));
 				
 			else if(DrinkingAlgorithm.Mood>=10)
-				label_2.setIcon(new ImageIcon(profA.class.getResource("../images/0-5.png")));
+				label_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("0-5.png")));
 			
 			if(Meter<5&&DrinkingAlgorithm.Mood>=10) {
 				lblNewLabel_2.setBounds(0, 0, 1280, 720);
-				lblNewLabel_2.setIcon(new ImageIcon(profA.class.getResource("../images/win1.png")));
-				GoodFeeling.teacher1FeelUp(); //±³¼ö aÈ£°¨µµ »ó½Â
-				btnNewButton.setVisible(false); //¸¶½Ã±â ¹öÆ° ¾Èº¸ÀÌ°ÔÇÑ´Ù
-				button.setVisible(false); //¾È¸¶½Ã±â ¹öÆ° ¾Èº¸ÀÌ°ÔÇÑ´Ù
-				button_2.setVisible(false); //³»¸®±â ¹öÆ° ¾Èº¸ÀÌ°ÔÇÑ´Ù
+				lblNewLabel_2.setIcon(new ImageIcon(getClass().getClassLoader().getResource("win1.png")));
+				Feeling.pf1_UP(); //êµìˆ˜ aí˜¸ê°ë„ ìƒìŠ¹
+				btnNewButton.setVisible(false); //ë§ˆì‹œê¸° ë²„íŠ¼ ì•ˆë³´ì´ê²Œí•œë‹¤
+				button.setVisible(false); //ì•ˆë§ˆì‹œê¸° ë²„íŠ¼ ì•ˆë³´ì´ê²Œí•œë‹¤
+				button_2.setVisible(false); //ë‚´ë¦¬ê¸° ë²„íŠ¼ ì•ˆë³´ì´ê²Œí•œë‹¤
 				
-				button_1.setVisible(true); //°á°úÈ­¸éÀ¸·Î ³Ñ¾î°¡´Â ¹öÆ° º¸ÀÌ°ÔÇÑ´Ù
+				button_1.setVisible(true); //ê²°ê³¼í™”ë©´ìœ¼ë¡œ ë„˜ì–´ê°€ëŠ” ë²„íŠ¼ ë³´ì´ê²Œí•œë‹¤
 				}
 			}
 		});
@@ -222,8 +185,8 @@ public class profA extends JPanel {
 		
 		
 		
-		JButton btnNewButton_1 = new JButton("\uC219\uCDE8\uD574\uC18C\uC74C\uB8CC");
-		btnNewButton_1.setIcon(new ImageIcon(profA.class.getResource("../images/\uD5DB\uAC1C\uC218_2.jpg")));
+		final JButton btnNewButton_1 = new JButton("\uC219\uCDE8\uD574\uC18C\uC74C\uB8CC");
+		btnNewButton_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("\uD5DB\uAC1C\uC218_2.jpg")));
 		
 		
 		btnNewButton_1.addActionListener(new ActionListener() {
@@ -233,23 +196,20 @@ public class profA extends JPanel {
 				
 				DrinkingAlgorithm.HangoverCure=true;
 				
-				
 				DrinkingAlgorithm.DrunkMeter=DrinkingAlgorithm.Drink(a);
 				
 			if(DrinkingAlgorithm.DrunkMeter>=0&&DrinkingAlgorithm.DrunkMeter<5) 
-					lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/0-5.png")));
+					lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("0-5.png")));
 					
-		
 			else if(DrinkingAlgorithm.DrunkMeter>=5&&DrinkingAlgorithm.DrunkMeter<10)
-			
-					lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/5-10.png")));
+					lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("5-10.png")));
 			
 			else if(DrinkingAlgorithm.DrunkMeter>=10&&DrinkingAlgorithm.DrunkMeter<15) 
-				lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/10-15.png")));
+				lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("10-15.png")));
 					
 			
 			else if(DrinkingAlgorithm.DrunkMeter>=15&&DrinkingAlgorithm.DrunkMeter<20) 
-				lblNewLabel_1.setIcon(new ImageIcon(profA.class.getResource("../images/19.png")));
+				lblNewLabel_1.setIcon(new ImageIcon(getClass().getClassLoader().getResource("19.png")));
 			
 			btnNewButton_1.setVisible(false);
 			}
@@ -263,19 +223,15 @@ public class profA extends JPanel {
 		label.setBounds(1055, 253, 213, 55);
 		add(label);
 		
-		
-		
 		JLabel label_1 = new JLabel("\uAD50\uC218\uB2D8 \uAE30\uBD84");
 		label_1.setHorizontalAlignment(SwingConstants.CENTER);
 		label_1.setBounds(78, 253, 213, 55);
 		add(label_1);
 		
-		
-		lblNewLabel.setIcon(new ImageIcon(profA.class.getResource("../images/\uC220\uC790\uB9AC(\uC548\uB9C8\uC2EC)_A.jpg")));//******
-		lblNewLabel.setFont(new Font("¸¼Àº °íµñ", Font.PLAIN, 16));
+		lblNewLabel.setIcon(new ImageIcon(getClass().getClassLoader().getResource("\uC220\uC790\uB9AC(\uC548\uB9C8\uC2EC)_A.jpg")));//******
+		lblNewLabel.setFont(new Font("ë§‘ì€ ê³ ë”•", Font.PLAIN, 16));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(0, 0, 1280, 720);
 		add(lblNewLabel);
-
 	}
 }
